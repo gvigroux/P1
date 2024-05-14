e881fe61a662d54593e09cc494fc2cc9d5fd169f
gvigroux <gvigroux@fee.fr>
2024-05-14 22:20:52 +0200

    0.0.10

--- app/api/[id]/easyRGPD.js/route.ts
+++ app/api/[id]/easyRGPD.js/route.ts
@@ -1,9 +1,21 @@
 import { NextResponse } from "next/server";
+import prisma from "@/prisma/db";
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }) {
   
   const id = params.id
-  return NextResponse.json({ message: "Hello World: " + id }, { status: 200 });
+  //return NextResponse.json({ message: "Hello World: " + id }, { status: 200 });
+
+  const headers = new Headers();
+  headers.append("Content-Type","text/javascript");
+  const options = { status: 200, statusText: "Success", headers};
+
+
+  const config = await prisma.Config.findFirst();
+
+
+  const response = new Response(config.data, options);
+  return response;
 }
